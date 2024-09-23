02da38db3cc039d5c235e855487b5df774e67527
haifeng2777 <107272252+haifeng2777@users.noreply.github.com>
2024-09-24 01:29:34 +0800

    Update 天天综合[密].js

--- js/天天综合[密].js
+++ js/天天综合[密].js
@@ -1,27 +1,22 @@
 var rule = {
   title: '天天综合[密]',//发布页:'https://666sav.com','https://771267398.550xx.vip',
-  hostJs: 'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,".login-txt&&a:eq(1)&&href");print(src);HOST=src',
-  host: 'https://666sav.com',
+  host: 'https://30kknn.vip',
   url: '/fyclass-fypage.html[/fyclass]',
-  searchUrl: '/sos**/page/fypage.html',
+  searchUrl: '/sos**/page/fypage.html[/sos**.html]',
   searchable: 2,
   headers: {'User-Agent': 'PC_UA'},
   hikerListCol: "movie_3",
   hikerClassListCol: "movie_3",
-  timeout: 5000,
-  filterable: 1,
-  class_name: '中文资源&超清资源&AV解说&国产传媒&久久热&废材资源&实时直播',
-  class_url: 'xjzy/&cq/&avjs/&gccm/&99re/&fcw/&zb/',
+  timeout: 5000,double: true,limit:5,
+  class_name: '中文1️⃣&超清2️⃣&AV解说3️⃣&传媒4️⃣&久久热5️⃣&废材网6️⃣&直播7️⃣&解说原片1️⃣&3D同人1️⃣&中文无码1️⃣&中文综合1️⃣&中文近亲1️⃣&中文护士1️⃣&中文师生1️⃣&中文强奸1️⃣&明星淫梦1️⃣&国产自拍1️⃣&三级伦理1️⃣&亚洲AV2️⃣&自偷拍2️⃣&传媒2️⃣&3D动漫2️⃣&欧美AV2️⃣&乱伦人妻2️⃣&丝袜制服2️⃣&直播录播2️⃣&SM另类2️⃣&超清三级2️⃣&水果解说3️⃣&综合传媒4️⃣&91制片4️⃣&杏吧原创4️⃣&糖心Vlgo4️⃣&天美传媒4️⃣&蜜桃传媒4️⃣&星空传媒4️⃣&精东影业4️⃣&兔子先生4️⃣&大象传媒4️⃣&国产自拍5️⃣&欧美5️⃣&中文字幕5️⃣&李宗瑞5️⃣&日本无码5️⃣&加勒比5️⃣&日本有码5️⃣&一本道5️⃣&高清5️⃣&潮吹5️⃣&会员认证5️⃣&制服丝袜5️⃣&口爆颜射5️⃣&肛交5️⃣&东京热5️⃣&小格式综合5️⃣&主播系列5️⃣&成人动漫5️⃣&SM性虐5️⃣&韩国综合5️⃣&VIP专区6️⃣&偷拍系列6️⃣&国产自拍6️⃣&日本有码6️⃣&日本无码6️⃣&成人动漫6️⃣&韩国综合6️⃣&VR专区6️⃣&欧美6️⃣&亚洲直播7️⃣&中文直播7️⃣&日本直播7️⃣&越南直播7️⃣&欧美直播7️⃣&俄国直播7️⃣&乌克兰直播7️⃣&卡哇伊直播7️⃣&咪狐直播7️⃣&蜜桃直播7️⃣&小妲己直播7️⃣&LOVE直播7️⃣&番茄直播7️⃣&77直播7️⃣&依依直播7️⃣&日出直播7️⃣&彩虹直播7️⃣&久久直播7️⃣&亚米直播7️⃣&蝶恋直播7️⃣&夜妖姬直播7️⃣&套路直播7️⃣&樱花直播7️⃣&享色直播7️⃣&红浪漫直播7️⃣&金鱼直播7️⃣&桃花直播7️⃣&花房直播7️⃣&小仙女直播7️⃣&视觉秀直播7️⃣&小天使直播7️⃣&彩云直播7️⃣&咪咪直播7️⃣&娇媚直播7️⃣&黄瓜直播7️⃣&色趣直播7️⃣&糯米直播7️⃣&小蜜蜂直播7️⃣&小红帽直播7️⃣&桃花运直播7️⃣&苦瓜直播7️⃣&爱爱你直播7️⃣&樱花雨直播7️⃣&盘他直播7️⃣&夜色直播7️⃣&蝴蝶直播7️⃣&小天仙直播7️⃣&杏趣直播7️⃣&小坏蛋直播7️⃣&飘雪直播7️⃣&樱桃直播7️⃣&奥斯卡直播7️⃣&兔女郎直播7️⃣&花仙子直播7️⃣&小性感直播7️⃣',class_url: 'xjzy/&cq/&avjs/&gccm/&99re/&fcw/&zb/&xjzy/cn-jieshuoyuanpian&xjzy/cn-3Dtongren&xjzy/cn-zhongwenwuma&xjzy/cn-zhongwenzonghe&xjzy/cn-zhongwenjingqing&xjzy/cn-zhongwenhushi&xjzy/cn-zhongwenshisheng&xjzy/cn-zhongwenqiangjian&xjzy/cn-mingxingyinmeng&xjzy/cn-guochanzipai&xjzy/cn-sanjizonghe&cq/2k-yazhou&cq/2k-zipaitoupai&cq/2k-chaoqingchuanmei&cq/2k-3Ddongman&cq/2k-oumei&cq/2k-luanlunrenqi&cq/2k-siwazhifu&cq/2k-zhibolubo&cq/2k-SMlinglei&cq/2k-chaoqingsanji&avjs/sgp-AVJS&gccm/91-madouyuanchuang&gccm/91-91zhipianchang&gccm/91-xingbayuanchuang&gccm/91-tangxinVlgo&gccm/91-tianmeichuanmei&gccm/91-mitaochuanmei&gccm/91-xingkongchuanmei&gccm/91-jingdongyingye&gccm/91-tuzixiansheng&gccm/91-daxiangchuanmei&99re/kdw-guochanzipai&99re/kdw-oumei&99re/kdw-zhongwenzimu&99re/kdw-lizongruiquanji&99re/kdw-ribenwuma&99re/kdw-jialebi&99re/kdw-ribenyouma&99re/kdw-yibendao&99re/kdw-gaoqing&99re/kdw-chaochui&99re/kdw-jiujiurehuiyuanrenzhengzuopin&99re/kdw-zhifusiwa&99re/kdw-koubaoyanshe&99re/kdw-gangjiao&99re/kdw-dongjingre&99re/kdw-xiaogeshizonghe&99re/kdw-hanguonvzhuboxilie&99re/kdw-chengrendongman&99re/kdw-SMxingnue&99re/kdw-hanguozonghe&fcw/ff-VIPzhuanqu&fcw/ff-toupaixilie&fcw/ff-guochanzipai&fcw/ff-ribenyouma&fcw/ff-ribenwuma&fcw/ff-chengrendongman&fcw/ff-hanguozonghe&fcw/ff-VRzhuanqu&fcw/ff-oumei&zb/?ju=yzzb/zb/gwzb&zb/?ju=yzzb/zb/cnzb&zb/?ju=yzzb/zb/ryzb&zb/?ju=yzzb/zb/yyzb&zb/?ju=yzzb/zb/omzb&zb/?ju=yzzb/zb/egzb&zb/?ju=yzzb/zb/wkl&zb/?ju=xxj/zb/kwy&zb/?ju=xxj/zb/mh&zb/?ju=xxj/zb/mt&zb/?ju=xxj/zb/xdj&zb/?ju=xxj/zb/love&zb/?ju=xxj/zb/fq&zb/?ju=xxj/zb/qq&zb/?ju=xxj/zb/yy&zb/?ju=xxj/zb/rc&zb/?ju=xxj/zb/ch&zb/?ju=xxj/zb/jj&zb/?ju=xxj/zb/ym&zb/?ju=xxj/zb/ld&zb/?ju=xxj/zb/yyj&zb/?ju=xxj/zb/tl&zb/?ju=xxj/zb/yh&zb/?ju=xxj/zb/xs&zb/?ju=xxj/zb/hlm&zb/?ju=xxj/zb/jy&zb/?ju=xxj/zb/th&zb/?ju=xxj/zb/hf&zb/?ju=xxj/zb/xxn&zb/?ju=xxj/zb/sjx&zb/?ju=xxj/zb/xts&zb/?ju=xxj/zb/cy&zb/?ju=xxj/zb/mm&zb/?ju=xxj/zb/jm&zb/?ju=xxj/zb/hg&zb/?ju=xxj/zb/sq&zb/?ju=xxj/zb/nm&zb/?ju=xxj/zb/xmf&zb/?ju=xxj/zb/xhm&zb/?ju=xxj/zb/thy&zb/?ju=xxj/zb/kg&zb/?ju=xxj/zb/aan&zb/?ju=xxj/zb/yh&zb/?ju=xxj/zb/pt&zb/?ju=xxj/zb/ys&zb/?ju=xxj/zb/hd&zb/?ju=xxj/zb/xtx&zb/?ju=xxj/zb/xq&zb/?ju=xxj/zb/xhd&zb/?ju=xxj/zb/px&zb/?ju=xxj/zb/yt&zb/?ju=xxj/zb/ask&zb/?ju=xxj/zb/tnl&zb/?ju=xxj/zb/hxz&zb/?ju=xxj/zb/xxg',
   play_parse: true,
-  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 0,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input;\n  }",
+  lazy: "js:\n  let html = request(input);\n  let hconf = html.match(/r player_.*?=(.*?)</)[1];\n  let json = JSON5.parse(hconf);\n  let url = json.url;\n  if (json.encrypt == '1') {\n    url = unescape(url);\n  } else if (json.encrypt == '2') {\n    url = unescape(base64Decode(url));\n  }\n  if (/\\.(m3u8|mp4|m4a|mp3)/.test(url)) {\n    input = {\n      parse: 1,\n      jx: 0,\n      url: url,\n    };\n  } else {\n    input;\n  }",
   tab_rename: {'道长在线': '在线线路'},
-  double: true,
   推荐: '*',
   一级: '#list_videos_videos_watched_right_now_items .item;img&&alt;.lazy-load&&data-original;.duration&&Text;a&&href',
   二级:'*',
   搜索: '*',
 }
-
 
 
 
